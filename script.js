64c0ddd51cb389b1f651dc7c8171b3a09c3ab08a
Callum Shaw <callum0shaw.gmail.com>
2022-11-15 19:10:40 +0200

    feat: implement basic math functions

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 (function () {
   // STATE/VARIABLES
-
   let state = {
     history: [],
     result: '',
-    currExp: [1, ' + ', 1],
+    currExp: [],
+    currNum: [],
   };
+
   // SELECTORS/VARIABLES
   let history = document.querySelector('.calc-history');
   let result = document.querySelector('.calc-result');
@@ -14,6 +15,7 @@
   let AcButton = document.querySelector('.calc-AC');
   let DelButton = document.querySelector('.calc-Del');
   let equalsButton = document.querySelector('.calc-equals');
+
   // EVENT LISTENERS
   numberButtons.forEach((el) => el.addEventListener('click', handleNumber));
   functionButtons.forEach((el) => el.addEventListener('click', handleOperator));
@@ -28,15 +30,43 @@
   }
 
   function handleNumber(event) {
+    // if there is a result, clear history/result
+    if(state.result !== '') {
+      console.log('clear')
+      handleClear()
+    }
     state.history.push(event.target.innerText);
+    state.currNum.push(event.target.innerText);
     updateDisplay();
+    console.log(state);
   }
 
+  // TODO: Break up function
   function handleOperator(event) {
+    // Issue warning if no number before operator
+    if (state.history.length === 0) return warning(event);
+    // If there is a result, change history to new result
+    if(state.result !== '') {
+      state.history = []
+      state.history.push(`${state.result}`) 
+      state.currNum = state.result
+    }
+    
     const operator = event.target.innerText;
-    if (state.currExp.length === 2) state.currExp[1] = ` ${operator} `;
-    state.history.push(` ${operator} `);
+
+    state.currExp.push(currNum);
+    state.currNum = [];
+    state.currExp.push(operator);
+
+    // if last input is operator, override
+    const regex = /[^0-9]/
+    if (regex.test(state.history.at(-1))) {
+      state.history.splice(-1, 1, ` ${operator} `);
+    } else {
+      state.history.push(` ${operator} `);
+    }
     updateDisplay();
+    console.log(state);
   }
 
   function handleDel(event) {
@@ -46,20 +76,31 @@
 
   function handleClear(event) {
     state.history = [];
-    state.result = [];
+    state.result = '';
+    state.currExp = [];
+    state.currNum = [];
     updateDisplay();
   }
 
   function handleEquals(event) {
+    if(state.currNum.length === 0) return warning(event)
+    state.currExp.push(parseInt(state.currNum.join('')))
+    state.currNum = []
     const str = state.currExp.join('');
     console.log(str);
 
-    // WARNING: Using Function constructor like this does give user access to local scope. However, due to 
+    // WARNING: Using Function constructor like this does give user access to local scope. However, due to
     // the limited nature of this app, it will be quite safe. If expanding, solutions include running
     // a regex to filter the 'str' to ensure no malicious code.
     const calculate = Function(`'use strict'; return (${str})`);
 
     state.result = calculate();
     updateDisplay();
+    console.log(state);
   }
-})();
+
+  function warning(event) {
+    event.target.classList.add('calc-button-warning')
+    setTimeout(() => event.target.classList.remove('calc-button-warning'), 100)
+  }
+})()
