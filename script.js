e2e1d6f4a8f20b5d5c6dfc4b111a10dc15bd3c6e
Callum Shaw <callum0shaw.gmail.com>
2022-11-16 10:51:06 +0200

    feat: correctly chains expressions

--- script.js
+++ script.js
@@ -31,35 +31,42 @@
 
   function handleNumber(event) {
     // if there is a result, clear history/result
-    if(state.result !== '') {
-      console.log('clear')
-      handleClear()
+    console.log(state);
+    if (state.currExp.length === 0) {
+      handleClear();
     }
     state.history.push(event.target.innerText);
     state.currNum.push(event.target.innerText);
     updateDisplay();
-    console.log(state);
   }
 
-  // TODO: Break up function
+  // TODO: Break up/simplify function
   function handleOperator(event) {
     // Issue warning if no number before operator
     if (state.history.length === 0) return warning(event);
+
     // If there is a result, change history to new result
-    if(state.result !== '') {
-      state.history = []
-      state.history.push(`${state.result}`) 
-      state.currNum = state.result
+    if (state.result !== '') {
+      state.history = [];
+      state.history.push(`${state.result}`);
+      state.currNum = [state.result];
     }
-    
     const operator = event.target.innerText;
 
-    state.currExp.push(currNum);
+    state.currExp.push(parseInt(state.currNum.join('')));
     state.currNum = [];
+
+    // If if second operator, resolve current expression and set result of that as first number in a new expression - update history
+    if (state.currExp.length === 3) {
+      const answer = calculate(state.currExp.join(''));
+      state.history = [answer];
+      state.currExp = [answer];
+    }
+
     state.currExp.push(operator);
 
     // if last input is operator, override
-    const regex = /[^0-9]/
+    const regex = /[^0-9]/;
     if (regex.test(state.history.at(-1))) {
       state.history.splice(-1, 1, ` ${operator} `);
     } else {
@@ -83,24 +90,25 @@
   }
 
   function handleEquals(event) {
-    if(state.currNum.length === 0) return warning(event)
-    state.currExp.push(parseInt(state.currNum.join('')))
-    state.currNum = []
-    const str = state.currExp.join('');
-    console.log(str);
+    if (state.currNum.length === 0) return warning(event);
+    state.currExp.push(parseInt(state.currNum.join('')));
+    state.currNum = [];
 
-    // WARNING: Using Function constructor like this does give user access to local scope. However, due to
-    // the limited nature of this app, it will be quite safe. If expanding, solutions include running
-    // a regex to filter the 'str' to ensure no malicious code.
-    const calculate = Function(`'use strict'; return (${str})`);
-
-    state.result = calculate();
+    state.result = calculate(state.currExp.join(''));
     updateDisplay();
+    state.currExp = []
     console.log(state);
   }
 
   function warning(event) {
-    event.target.classList.add('calc-button-warning')
-    setTimeout(() => event.target.classList.remove('calc-button-warning'), 100)
+    event.target.classList.add('calc-button-warning');
+    setTimeout(() => event.target.classList.remove('calc-button-warning'), 100);
   }
-})()
+
+  // WARNING: Using Function constructor like this does give user access to local scope. However, due to
+  // the limited nature of this app, it will be quite safe. If expanding, solutions include running
+  // a regex to filter the 'str' to ensure no malicious code.
+  function calculate(str) {
+    return Function(`'use strict'; return (${str})`)();
+  }
+})();
